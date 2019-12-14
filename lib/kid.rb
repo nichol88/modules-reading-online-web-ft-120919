8464e3fbb74c2c8387ba381a77d8c37063b750e3
Brian Nicholls <nichol88@gmail.com>
2019-12-14 06:11:24 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,8 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
-  include Dance
-  extend MetaDancing
+  
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
